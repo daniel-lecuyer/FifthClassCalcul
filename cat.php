0f41bf937a19dbcdfa63186205befb6fd764effa
Daniel <dan.irestful@gmail.com>
2013-12-04 11:32:26 -0500

    Created Projects

---
+++ cat.php
@@ -0,0 +1,9 @@
+<?php
+$amountOfLot = 236;
+$lenght = 150;
+$streetLenght = $amountOfLot * $lenght;
+$return = $lenght * 14;
+$distance = ($amountOfLot * $lenght) + ($lenght * 14);
+echo PHP_EOL."The result is: ".$distance.PHP_EOL;
+
+ 
